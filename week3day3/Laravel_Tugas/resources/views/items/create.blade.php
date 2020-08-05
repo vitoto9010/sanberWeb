c5ca48fa0e2eb6aab4355d2dc486c9f16d3d9098
Vito Ramadhan <vitorama98@gmail.com>
2020-08-05 15:12:05 +0700

    week3day3 data table first push

---
+++ week3day3/Laravel_Tugas/resources/views/items/create.blade.php
@@ -0,0 +1,13 @@
+@extends('adminlte.master')
+
+@section('content')
+    <h2>Ini halaman create</h2>
+@endsection
+
+@push('scripts')
+<script>
+    var wrappers = document.getElementsByClassName("wrappers");
+    var items = ["ini", "contoh"];
+    console.log(items);
+</script>
+@endpush
---
+++ week3day3/Laravel_Tugas/resources/views/items/index.blade.php
@@ -0,0 +1,5 @@
+@extends('adminlte.master')
+
+@section('content')
+    <h1>Halo ini index!!</h1>
+@endsection
